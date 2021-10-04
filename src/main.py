63e3b7c5c863d802b040ceafb86eab61352e4c98
Ehern15 <erikhern15@gmail.com>
2021-10-04 19:32:25 -0400

    Made function for math

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+def problemOne():
+    # addition is + subtract is - multiply is * and division is / 
+    y=
+    return answer
+
 def main():
     print("hello world")
+    # print(problemOne())
+    variable=problemOne()
+    print(variable)
+
+
+
 
 if __name__ == "__main__":
     main()
